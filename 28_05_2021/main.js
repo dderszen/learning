e2170d64fb5cdd18ef1fa7a0bca2156fa60f51e5
dderszen <dd.derszen@gmail.com>
2021-05-29 14:51:37 +0200

    Finished PLN to foreign currency conversion.

--- 28_05_2021/main.js
+++ 28_05_2021/main.js
@@ -1,15 +1,29 @@
-const amount = document.querySelector('#amount');
 let currencySelector = document.querySelector('#currency');
 var url = 'http://api.nbp.pl/api/exchangerates/tables/C/?format=json';
-
+var arrCurrency = [];
+var arrValues = [];
 fetch(url)
     .then((res) => res.json())
     .then((data)=>{
         data[0].rates.forEach(function(rate){
+            arrCurrency.push(rate.code);
+            arrValues.push(rate.bid);
             var opt = document.createElement('option');
             opt.appendChild(document.createTextNode(rate.code));
-            opt.id = rate.code;
+            opt.id = rate.code; 
             currencySelector.appendChild(opt);
         });
         //console.log(data[0].rates[0].code);
     });
+
+
+    
+document.getElementById('exchange').addEventListener('click', (e)=>{
+    //console.log(currencySelector.value);
+    const amount = document.querySelector('#amount').value;
+    var index = arrCurrency.findIndex(x => x == currencySelector.value);
+    var exchangedValue = amount / arrValues[index];
+    document.getElementById("exchanged-amount").innerText = Math.round(exchangedValue * 100) / 100;
+    document.getElementById("currency-tag").innerText = currencySelector.value;
+    document.getElementById("eaContainer").style.opacity = '1';
+});
